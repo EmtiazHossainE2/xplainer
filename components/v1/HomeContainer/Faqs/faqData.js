5e8ad4fcff257c2ebe83aa368dfba9c0076fc53a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-10 21:35:16 +0600

    faq

---
+++ components/v1/HomeContainer/Faqs/faqData.js
@@ -0,0 +1,26 @@
+export const faqData = [
+  {
+    question:"How does the Program work?",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+  {
+    question:"I made the payment and didn't recieve any email?",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+  {
+    question:"Can I get a refund?",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+  {
+    question:"When can I receive the bonuses?",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+  {
+    question:"Will I get a certificate after completion?",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+  {
+    question:"How will I access the content",
+    answer:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio velit nihil ipsum ducimus ipsa iusto autem explicabo. Dolore, rerum."
+  },
+]
