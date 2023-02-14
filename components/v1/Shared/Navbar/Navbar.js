e07301d063676fcc1571679f0e1b4075abf09f86
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-14 17:57:29 +0600

    navbar responsive

--- components/v1/Shared/Navbar/Navbar.js
+++ components/v1/Shared/Navbar/Navbar.js
@@ -2,11 +2,15 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import CouponModal from '../Modal/CouponModal';
 import LeadModal from '../Modal/LeadModal';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { RiCloseLine } from 'react-icons/ri';
+import styles from '@/styles/Navbar.module.css'
 
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
   const [showModal, setShowModal] = useState(false)
   const [couponModal, setCouponModal] = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,30 +37,55 @@ const Navbar = () => {
         {/******************* Header Top Banner  ***************************/}
         <div className='flex justify-center p-4 bg-[#0070F4] '>
           <p
-            className="text-center text-xl font-bold text-white hover:cursor-pointer"
+            className="text-center text-[16px] md:text-xl font-bold text-white hover:cursor-pointer"
             onClick={() => setShowModal(true)}
           >
             🎉 Click here to unlock upto 50% discount offer 🎉{" "}
           </p>
         </div>
 
-        <div className={`container mx-auto  px-20 ${isSticky ? 'py-4' : 'pt-4 pb-8'}`}>
-          <div className='flex justify-between items-center'>
-            <div>
+        {/********************** * For Desktop  ********************* */}
+        <div className={`${styles.navbar} container mx-auto  px-20 ${isSticky ? 'py-4' : 'pt-4 pb-8'}`}>
+          <div className={`${styles.navbar__links}`}>
+            <div className={`${styles.navbar__logo}`}>
               <Link href='/'><h2 className='font-bold text-[30px]'>Xplainerr</h2></Link>
             </div>
-            <div className='flex'>
-              <p className='text-lg font-semibold px-5'><Link href='blog'>Blog</Link></p>
-              <p className='text-lg font-semibold px-5'><Link href='/'>API for PM</Link></p>
-              <p className='text-lg font-semibold px-5'><Link href='/'>Pricing</Link></p>
-              <p className='text-lg font-semibold px-5'><Link href='/'>FAQ</Link></p>
-              <p className='text-lg font-semibold px-5'><Link href='/'>Team</Link></p>
-              <p className='text-lg font-semibold '>
-                <Link href='/' className='bg-[#0070F4] rounded-[37px] py-[18px] px-6'>
-                  <button className=' text-white'>Get Free Ebook</button>
-                </Link>
-              </p>
-            </div>
+          </div>
+          <div className={`${styles.navbar__container}`}>
+            <p className='text-lg font-semibold px-5'><Link href='blog'>Blog</Link></p>
+            <p className='text-lg font-semibold px-5'><Link href='/'>API for PM</Link></p>
+            <p className='text-lg font-semibold px-5'><Link href='/'>Pricing</Link></p>
+            <p className='text-lg font-semibold px-5'><Link href='/'>FAQ</Link></p>
+            <p className='text-lg font-semibold px-5'><Link href='/'>Team</Link></p>
+          </div>
+          <div className={`${styles.navbar__sign}`}>
+            <Link href='/' className='bg-[#0070F4] rounded-[37px] py-[18px] px-6'>
+              <button className=' text-white text-lg font-semibold'>Get Free Ebook</button>
+            </Link>
+          </div>
+
+          {/********************** * For Mobile ********************* */}
+          <div className={`${styles.navbar__menu}`}>
+            {toggleMenu
+              ? <RiCloseLine className='cursor-pointer' size={27} onClick={() => setToggleMenu(false)} />
+              : <AiOutlineMenu className='cursor-pointer' size={27} onClick={() => setToggleMenu(true)} />
+            }
+            {toggleMenu && (
+              <div className={`scale-up-center ${styles.navbar__menuContainer} `}>
+                <div >
+                  <p className='text-lg font-semibold px-5'><Link href='blog'>Blog</Link></p>
+                  <p className='text-lg font-semibold px-5'><Link href='/'>API for PM</Link></p>
+                  <p className='text-lg font-semibold px-5'><Link href='/'>Pricing</Link></p>
+                  <p className='text-lg font-semibold px-5'><Link href='/'>FAQ</Link></p>
+                  <p className='text-lg font-semibold px-5'><Link href='/'>Team</Link></p>
+                </div>
+                <div className={`${styles.navbar__menuContainerLinksSign}`}>
+                  <Link href='/' className='bg-[#0070F4] rounded-[37px] py-[12px] lg:py-[18px] px-6'>
+                    <button className=' text-white text-lg font-semibold'>Get Free Ebook</button>
+                  </Link>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </header>
