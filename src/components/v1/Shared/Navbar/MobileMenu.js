b79235ef73ec0d8b886059abb015eb1741ea7e24
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-19 17:23:00 +0600

    navigation optimize

--- src/components/v1/Shared/Navbar/MobileMenu.js
+++ src/components/v1/Shared/Navbar/MobileMenu.js
@@ -17,22 +17,22 @@ const MobileMenu = () => {
   const [workShopsOpen, setWorkShopsOpen] = useState(false);
 
   return (
-    <div className={`block lg:hidden`}>
+    <div className={`block lg:hidden `}>
 
       <AiOutlineMenu className='cursor-pointer' size={27} onClick={() => setToggleMenu(true)} />
 
       {toggleMenu && (
-        <div 
-        className={`pt-2 pb-6 w-9/12 md:w-1/2 h-screen bg-white z-20 fixed top-0 -left-0 lg:left-0 lg:hidden  peer-focus:left-0 transition ease-out delay-150 duration-300 `} 
-        data-aos="fade-right"
-        data-aos-offset="100"
-        data-aos-duration="200"
-        data-aos-easing="ease-in-sine"
+        <div
+          className={`pt-2 pb-6 w-9/12  h-screen bg-white z-20 fixed top-0 -left-0 lg:left-0 lg:hidden  peer-focus:left-0 transition ease-out delay-150 duration-300 border-r border-[#a9a6a6] navdrawer `}
+          data-aos="fade-right"
+          data-aos-offset="100"
+          data-aos-duration="200"
+          data-aos-easing="ease-in-sine"
 
         >
           <div className="flex flex-col justify-start item-center">
             <div className="flex justify-between shadow-sm p-2">
-              <h3 className="font-bold text-2xl">Xplainerr</h3>
+              <h3 className="font-bold text-xl">Xplainerr</h3>
               <button onClick={() => setToggleMenu(false)}><AiOutlineCloseCircle size={28} /></button>
             </div>
 
@@ -40,7 +40,7 @@ const MobileMenu = () => {
             <div>
               <div className={`relative flex flex-col gap-5 pt-4`}>
 
-                <div className='text-lg font-semibold px-3 '>
+                <div className='text-md font-semibold px-3 '>
                   <div
                     onClick={() => {
                       setCourseOpen(!courseOpen)
@@ -62,7 +62,7 @@ const MobileMenu = () => {
                   </div>
                 </div>
 
-                <div className={`text-lg font-semibold px-3 ${courseOpen ? 'mt-52' : ''}`}>
+                <div className={`text-md font-semibold px-3 ${courseOpen ? 'mt-40' : ''}`}>
                   <div
                     onClick={() => {
                       setWorkShopsOpen(!workShopsOpen)
@@ -86,36 +86,35 @@ const MobileMenu = () => {
                 </div>
                 {/* Dropdown  */}
                 {courseOpen && (
-                  <div onClick={() => setCourseOpen(false)} className="absolute left-0 top-10 z-10 bg-white py-2 mt-2 shadow-xl">
+                  <div onClick={() => setCourseOpen(false)} className="absolute left-0 top-10 z-10 bg-white py-2 mt-2 shadow-lg w-full">
 
                     <Link href='/courses/api-for-pm' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={apiForPmSvg} alt='api for pm icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={apiForPmSvg} alt='api for pm icon' width={20} height={20} />
                         <div className=''>
-                          <h4 className='font-semibold text-sm'>API for Product Manager</h4>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
+                          <h4 className='font-medium text-[12px]'>API for Product Manager</h4>
+
                         </div>
                       </div>
                     </Link>
 
                     <Link href='/courses/pricing-for-pm' className="block my-2 pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={pricing} alt='pricing icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={pricing} alt='pricing icon' width={20} height={20} />
                         <div className=''>
-                          <h4 className='font-semibold text-sm'>A to Z of Pricing & Monetization</h4>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
+                          <h4 className='font-medium text-[12px]'>A to Z of Pricing & Monetization</h4>
+
                         </div>
                       </div>
                     </Link>
 
                     <Link href='/courses/user-interview' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={users} alt='user icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={users} alt='user icon' width={20} height={20} />
                         <div className=''>
-                          <div className="flex gap-x-3 items-center ">
-                            <h4 className='font-medium text-sm'>  How to user interviews  </h4>
+                          <div className="flex gap-x-2 items-center ">
+                            <h4 className='font-medium text-[12px]'>  How to user interviews  </h4>
                           </div>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
                         </div>
                       </div>
                     </Link>
@@ -124,36 +123,33 @@ const MobileMenu = () => {
                 )}
 
                 {workShopsOpen && (
-                  <div onMouseLeave={() => setWorkShopsOpen(false)} className="absolute left-0 top-20 z-10 bg-white py-2 mt-2 shadow-xl">
+                  <div onMouseLeave={() => setWorkShopsOpen(false)} className="absolute left-0 top-20 z-10 bg-white py-2 mt-2 shadow-lg w-full">
 
                     <Link href='/workshops/no-code' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={noCode} alt='icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={noCode} alt='icon' width={20} height={20} />
                         <div className=''>
-                          <h4 className='font-semibold'>No Code </h4>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
+                          <h4 className='font-medium text-[12px]'>No Code </h4>
                         </div>
                       </div>
                     </Link>
 
                     <Link href='/workshops/product-hunt' className="block my-2 pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={productHunt} alt='product hunt icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={productHunt} alt='product hunt icon' width={20} height={20} />
                         <div className=''>
-                          <h4 className='font-semibold'>Product Hunt Launch</h4>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
+                          <h4 className='font-medium text-[12px]'>Product Hunt Launch</h4>
                         </div>
                       </div>
                     </Link>
 
                     <Link href='/workshops/build-brand' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
-                      <div className='flex items-center gap-x-3 imgIcon'>
-                        <Image src={personalBrand} alt='personal brand icon' width={30} height={27} />
+                      <div className='flex items-center gap-x-2 imgIcon'>
+                        <Image src={personalBrand} alt='personal brand icon' width={20} height={20} />
                         <div className=''>
                           <div className="flex gap-x-2 items-center">
-                            <h4 className='font-semibold'>  Build Your Personal Brand  </h4>
+                            <h4 className='font-medium text-[12px]'>  Build Your Personal Brand  </h4>
                           </div>
-                          <p className='text-[12px] text-[#515151]'>Top rated. Beginner friendly.</p>
                         </div>
                       </div>
                     </Link>
@@ -162,19 +158,19 @@ const MobileMenu = () => {
                 )}
 
                 <p
-                  className={`text-lg font-semibold px-3 ${workShopsOpen ? 'mt-52' : ''}`}
-                  onMouseOver={() => {
-                    setWorkShopsOpen(false)
-                    setCourseOpen(false)
+                  className={`text-md font-semibold px-3 ${workShopsOpen ? 'mt-36' : ''}`}
+                  onClick={() => {
+                    setWorkShopsOpen(!workShopsOpen)
+                    setCourseOpen(!courseOpen)
                   }}
                 >
                   <Link href='/mock-interview' className=''>Mock Interviews</Link>
                 </p>
-                <p className='text-lg font-semibold px-3 mb-5' ><Link href='/blog' className=''>Blog</Link></p>
+                <p className='text-md font-semibold px-3 mb-5' ><Link href='/blog' className=''>Blog</Link></p>
               </div>
               <div className={`px-3 `}>
                 <Link href='/login' className='bg-[#0070F4] rounded-md py-[10px] px-[51px]'>
-                  <button className=' text-white text-lg font-semibold'>Login</button>
+                  <button className=' text-white text-md font-semibold'>Login</button>
                 </Link>
               </div>
             </div>
@@ -182,6 +178,8 @@ const MobileMenu = () => {
           </div>
         </div>
       )}
+
+
     </div>
   )
 }
