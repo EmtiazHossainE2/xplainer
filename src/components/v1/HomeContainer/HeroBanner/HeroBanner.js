b43d88be89aaf8591862443da01a7fa1cb9d7340
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-23 00:42:26 +0600

    optimize herobanner

--- src/components/v1/HomeContainer/HeroBanner/HeroBanner.js
+++ src/components/v1/HomeContainer/HeroBanner/HeroBanner.js
@@ -8,7 +8,7 @@ const HeroBanner = () => {
         {/* Left  */}
         <div className='basis-1/2 text-center lg:text-start'>
           <h2 className="text-3xl lg:text-[40px] 2xl:text-5xl font-bold pb-5 md:py-5">Accelerate your <br /> tech career</h2>
-          <p className='text-gray-500 text-lg lg:text-xl 2xl:text-2xl '>We provide niche up skilling courses to help your accelerate and succeed in tech career</p>
+          <p className='text-gray-500 text-lg lg:text-xl 2xl:text-2xl  '>We provide niche up skilling courses to help your accelerate and succeed in tech career</p>
           <div className="pt-12">
             <Link href='/courses' className="bg-[#0070F4] rounded-[4px] py-[18px] px-6">
               <button className='text-white font-[500]'>Explore Courses</button>
@@ -17,7 +17,7 @@ const HeroBanner = () => {
         </div>
 
         {/* Right */}
-        <div className='basis-1/2 flex justify-center items-center'>
+        <div className='basis-1/2 hidden md:block lg:flex justify-center items-center'>
           <Image
             src="/images/shared/hero1.jpg"
             alt="Hero Banner"
--- src/components/v1/Shared/Navbar/Navbar.js
+++ src/components/v1/Shared/Navbar/Navbar.js
@@ -69,7 +69,7 @@ const Navbar = () => {
                   setCourseOpen(true)
                   setWorkShopsOpen(false)
                 }}
-                className={`flex justify-between items-center cursor-pointer gap-x-2 ${courseOpen ? "border-b-2 border-[#0070F4] " : ""}`}
+                className={`flex justify-between items-center cursor-pointer gap-x-2 ${courseOpen ? "border-b-2 pb-[6px] border-[#0070F4] " : ""}`}
               >
                 Courses
                 <div className="flex justify-between items-center  gap-x-1">
@@ -90,7 +90,7 @@ const Navbar = () => {
                   setWorkShopsOpen(true)
                   setCourseOpen(false)
                 }}
-                className={`flex justify-between cursor-pointer items-center gap-x-2 ${workShopsOpen ? "border-b-2 border-[#0070F4] " : ""}`}
+                className={`flex justify-between cursor-pointer items-center gap-x-2 ${workShopsOpen ? "border-b-2  pb-[6px] border-[#0070F4] " : ""}`}
               >
                 Workshops
                 <div className="flex justify-between items-center  gap-x-1">
@@ -108,7 +108,7 @@ const Navbar = () => {
             </div>
             {/* Dropdown  */}
             {courseOpen && (
-              <div onMouseLeave={() => setCourseOpen(false)} className="absolute left-3 top-5 z-10 bg-white py-2 mt-2 shadow-xl">
+              <div onMouseLeave={() => setCourseOpen(false)} className="absolute left-3 top-8 z-10 bg-white py-2 mt-2 shadow-xl">
 
                 <Link href='/courses/api-for-pm' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
                   <div className='flex gap-x-3 '>
@@ -147,7 +147,7 @@ const Navbar = () => {
             )}
 
             {workShopsOpen && (
-              <div onMouseLeave={() => setWorkShopsOpen(false)} className="absolute right-2 top-5 z-10 bg-white py-2 mt-2 shadow-xl">
+              <div onMouseLeave={() => setWorkShopsOpen(false)} className="absolute right-2 top-8 z-10 bg-white py-2 mt-2 shadow-xl">
 
                 <Link href='/workshops/no-code' className="block pl-4 pr-8 py-2 hover:bg-[#EAFCFF]  hover:text-[#006BC2]">
                   <div className='flex gap-x-3 '>
