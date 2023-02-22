f39a10527d26f5c10b58e185f9530a104bc3e29b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-22 22:52:08 +0600

    timeline update

--- src/components/v1/HomeContainer/Mentors/Mentor.js
+++ src/components/v1/HomeContainer/Mentors/Mentor.js
@@ -7,15 +7,31 @@ const Mentor = ({ mentor }) => {
         <Image src={`/images/mentors/${mentor.image}`} width={310} height={250} alt={`${mentor.name}`} />
       </div>
       <h4 className="text-center text-xl font-semibold py-2">{mentor.name}</h4>
-      {/* Job Road Map  */}
-      <div className="roadMap text-left pt-2">
-        <div className="roadMap_dataBox box">
-          <div className="singleData-box">
-            <p className="list">{mentor.job1}</p>
-            <p className="list">{mentor.job2}</p>
+      
+      {/* Job Timeline  */}
+      <ul>
+        <li class="relative flex items-baseline gap-3 pb-3">
+          <div class="before:absolute before:left-[3.2px] before:h-full before:w-[1px] before:bg-[#B6B6B6]">
+            <svg xmlns="http://www.w3.org/2000/svg" width="7" height="7" class="bi bi-circle-fill fill-[#B6B6B6]" viewBox="0 0 16 16">
+              <circle cx="8" cy="8" r="8" />
+            </svg>
           </div>
-        </div>
-      </div>
+          <div>
+            <p className="text-sm">{mentor.job1}</p>
+          </div>
+        </li>
+        <li class="relative flex items-baseline gap-3 pb-5">
+          <div class="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="7" height="7" class="bi bi-circle-fill fill-[#B6B6B6]" viewBox="0 0 16 16">
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+          </div>
+          <div>
+            <p className="text-sm">{mentor.job2}</p>
+          </div>
+        </li>
+      </ul>
+
     </div>
   )
 }
