293d93783ba9fc09a2087884984662acc022387e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-02-24 01:22:39 +0600

    optimization

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,19 @@
 import CommonHead from "@/src/components/v1/Shared/CommonHead"
+import PageLayout from "@/src/layout/PageLayout"
 
 const Error = () => {
   return (
     <>
       <CommonHead
-        title={'XPlainerr | Comming Soon'}
+        title={'XPlainerr | Coming Soon'}
         metaDes={'description'}
         favIcon={'/favicon.ico'}
       />
-      <div className="flex justify-center items-center h-screen">
-        <h2 className="text-3xl lg:text-4xl 2xl:text-5xl font-bold">Coming Soon ...</h2>
-      </div>
+      <PageLayout>
+        <div className="flex justify-center items-center h-[50vh]">
+          <h2 className="text-3xl lg:text-4xl 2xl:text-5xl font-bold">Coming Soon ...</h2>
+        </div>
+      </PageLayout>
     </>
   )
 }
--- src/components/v1/HomeContainer/Brand/Brand.js
+++ src/components/v1/HomeContainer/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = () => {
   return (
     <div className='container mx-auto py-4 px-6 md:px-32'>
       <div className="flex flex-col justify-center items-center">
-        <h2 className="customTitle">Learners From Top Companies </h2>
+        <h2 className="customTitle xl:pt-16">Learners From Top Companies </h2>
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
@@ -56,80 +56,6 @@ const Brand = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        {/* <div className='py-8 flex flex-wrap justify-center items-center gap-10'>
-          <div>
-            <Image
-              src="/images/brand/brand1.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand2.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand3.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand4.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand5.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand6.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand7.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/brand/brand8.png"
-              alt=""
-              width={100}
-              height={80}
-              priority
-            />
-          </div>
-        </div> */}
       </div>
     </div>
   )
