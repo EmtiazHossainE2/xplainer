1da9328ed92b8b67ffd7275660e3283047ea45f6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-02 18:28:02 +0600

    header added

--- src/layout/SidebarLayout.js
+++ src/layout/SidebarLayout.js
@@ -1,16 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { FaLock } from 'react-icons/fa'
 import topBadge from '/public/images/courses/top-post-badge.svg'
 
 
 const SidebarLayout = ({ posts, children }) => {
+
   return (
     <>
-      <div className="relative">
+      {/********************** Header Top  **********************/}
+      <header className='bg-[#001934] text-white shadow-md fixed w-full z-10 top-0 border-b border-gray-200 px-16'>
+        <div className="flex justify-between">
+          <Link href='/'><h2 className='font-bold text-[26px] py-2'>Xplainerr</h2></Link>
+          <div className="flex justify-center items-center gap-x-6">
+            <Link href='/pm-question'><h4 className="font-semibold hover:border-b-2">PM Interview Questions</h4></Link>
+            <Link href='/login'><h4 className="font-semibold hover:border-b-2">Login</h4></Link>
+            <Link href='/buy-now'><button className="px-3 py-1.5 bg-[#B80C07] rounded-md ">Buy Now</button></Link>
+          </div>
+        </div>
+      </header>
+
+      {/********************** Main Body **********************/}
+      <div className="relative hidden lg:block">
         <div className="flex">
           {/********************* Left Side  **********************/}
-          <div className="flex flex-col text-left fixed top-10 left-0 w-[18%] min-h-[100vh] px-2 shadow-2xl gap-8">
+          <div className="flex flex-col text-left fixed top-16 left-0 w-[18%] min-h-[100vh] px-2 shadow-2xl gap-8">
             <div>
               {posts &&
                 posts.map((post, index) => (
@@ -28,12 +43,12 @@ const SidebarLayout = ({ posts, children }) => {
           </div>
 
           {/********************** Content  **********************/}
-          <div className="p-8 ml-[20%] mr-[18%]">
+          <div className="px-8 py-16 ml-[20%] mr-[18%]">
             {children}
           </div>
 
           {/********************** Right Side  **********************/}
-          <div className="flex flex-col text-left fixed top-10 right-0 w-[15%] min-h-[100vh] pl-2">
+          <div className="flex flex-col text-left fixed top-16 right-0 w-[15%] min-h-[100vh] pl-2">
             <div className="border-l pl-3">
               <p className="text-lg pb-3">Content</p>
               <div className="text-sm flex flex-col gap-3">
@@ -46,6 +61,9 @@ const SidebarLayout = ({ posts, children }) => {
           </div>
         </div>
       </div>
+
+
+      
 
     </>
   );
