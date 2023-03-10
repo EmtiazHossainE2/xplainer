e6aa52088cefe601aba225c56d6c358620f847f2
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-10 23:37:42 +0600

    menu

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,32 @@
 import auth from '@/src/auth/firebase/Firebase.init'
+import MyCourses from '@/src/components/v1/Dashboard/MyCourses'
+import Settings from '@/src/components/v1/Dashboard/Settings'
+import WishList from '@/src/components/v1/Dashboard/WishList'
 import CommonHead from '@/src/components/v1/Shared/CommonHead'
 import DashboardLayout from '@/src/layout/DashboardLayout'
 import Image from 'next/image'
+import { useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 const Dashboard = () => {
   const [user] = useAuthState(auth)
   // console.log(user)
+
+  const [active, setActive] = useState(0);
+  const [clicked, setClicked] = useState(0);
+
+  const menus = [
+    { id: 0, name: "All Courses" },
+    { id: 1, name: "Wishlist" },
+    { id: 2, name: "Settings" },
+  ];
+
+  const handleActive = (index) => {
+    setActive(index);
+    setClicked(index);
+  };
+
+
   return (
     <>
       <CommonHead
@@ -15,15 +35,37 @@ const Dashboard = () => {
         favIcon={"/favicon.ico"}
       />
       <DashboardLayout user={user}>
-        <div className='container mx-auto px-3 lg:px-16'>
-          <h2 className='text-2xl lg:text-3xl font-semibold py-3 lg:pt-5'>Welcome Back ,
-            <span className='lg:hidden pb-1'><br /></span>
-            {user?.displayName}</h2>
-          <p className='text-gray-500'>Continue learning with our recommendations based on your career goals and recent activity</p>
-          <div className='pt-5'>
-            <Image className='rounded' src={user?.photoURL} width={300} height={300} alt="user photo" />
+        <div className="bg-black">
+          <div className='container mx-auto px-3 lg:px-16 pt-5 pb-.5'>
+            <h2 className='text-2xl  text-white font-semibold py-3 lg:pt-5'>Welcome Back , {" "}
+              <span className='lg:hidden pb-1'><br /></span>
+              {user?.displayName}</h2>
+
+            <h2 className='text-2xl  lg:text-3xl text-white font-semibold py-3 lg:py-10'>My Learning </h2>
+            {/* Menus  */}
+            <nav className="flex items-center gap-5 border-gray-200 ">
+              {menus.map((menu, index) => (
+                <p
+                  key={index}
+                  onClick={() => handleActive(index)}
+                  className={`text-xl cursor-pointer pb-2 px-2 border-b-[6px]  ${index === clicked ? "text-white font-semibold " : "border-transparent text-white"
+                    }`}
+                >
+                  {menu.name}
+                </p>
+              ))}
+            </nav>
+            {/* Menus  */}
           </div>
         </div>
+
+        {/* Child  */}
+        <div className="container mx-auto px-3 lg:px-16 my-5">
+          {active === 0 && <MyCourses />}
+          {active === 1 && <WishList />}
+          {active === 2 && <Settings />}
+        </div>
+
       </DashboardLayout>
     </>
   )
