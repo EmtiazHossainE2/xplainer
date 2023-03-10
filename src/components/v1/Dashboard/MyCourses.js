11ad398d2b8d8355655740bfa91022ed4020f842
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-11 00:27:12 +0600

    fix

--- src/components/v1/Dashboard/MyCourses.js
+++ src/components/v1/Dashboard/MyCourses.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 const MyCourses = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:mb-56 2xl:mb-72">
         {myCourses &&
           myCourses.map((course, index) => (
             <div key={index}>
