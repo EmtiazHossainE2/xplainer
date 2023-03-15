2eccbdf1f7d49a7ad07bf0e5948a4cd66b55442c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-15 19:08:05 +0600

    faq section done

--- src/components/v1/HomeContainer/Faqs/Faqs.js
+++ src/components/v1/HomeContainer/Faqs/Faqs.js
@@ -11,7 +11,7 @@ const Faqs = () => {
   };
 
   return (
-    <div className='container mx-auto section__padding '>
+    <div className='container mx-auto px-5 lg:px-36 large:px-96 '>
       <div className="p-2 lg:px-32">
         {faqData.map((faq, index) => (
           <div key={index} className="mb-3 border-b  pt-2 pb-4 px-2 rounded-md">
@@ -33,6 +33,10 @@ const Faqs = () => {
             </div>
           </div>
         ))}
+        <div className="text-center py-10 hidden md:block">
+          <button className="bg-[#0070F4] py-5 px-16 rounded-[46px] text-white font-semibold">Unleash the Power of Content Creation for just Rs.2999! ₹7,999</button>
+          <p className="text-sm text-[#475467] pt-4 ">Reserve a seat before April 5, 2023 to unlock Bonuses worth ₹25,000</p>
+        </div>
       </div>
     </div>
   )
