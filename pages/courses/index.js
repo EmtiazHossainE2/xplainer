a8ffafdb2ea4973766af57056059cf53c8a0a575
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-03-19 00:13:39 +0600

    all courses page done

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,6 +1,7 @@
-import { Courses } from '@/src/components/v1/HomeContainer'
+import AllCourse from '@/src/components/v1/Courses/AllCourse/AllCourse'
 import CommonHead from '@/src/components/v1/Shared/CommonHead'
 import PageLayout from '@/src/layout/PageLayout'
+import Image from 'next/image'
 import { withRouter } from 'next/router'
 
 const AllCourses = () => {
@@ -13,12 +14,17 @@ const AllCourses = () => {
       />
       <main>
         <PageLayout>
-          <div className="flex flex-col min-h-screen overflow-hidden">
-
-            <div className="grow">
-            <Courses heading={'All Courses'} ctaText={'Buy Now'} />
+          <div className=''>
+            <div className="container mx-auto px-5 lg:px-12 big:px-28 large:px-96">
+              <div className='flex justify-center pt-[70px] pb-16'>
+                <Image src='/images/courses/banner.png' width={1200} height={180} alt='banner' />
+              </div>
+              <div>
+                <AllCourse />
+              </div>
             </div>
           </div>
+
         </PageLayout>
       </main>
     </>
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,33 +1,33 @@
 export const topCompanies = [
   {
-    logo : '/1.svg',
+    logo: '/1.svg',
   },
   {
-    logo : '/2.svg',
+    logo: '/2.svg',
   },
   {
-    logo : '/3.svg',
+    logo: '/3.svg',
   },
   {
-    logo : '/4.svg',
+    logo: '/4.svg',
   },
   {
-    logo : '/5.svg',
+    logo: '/5.svg',
   },
   {
-    logo : '/9.svg',
+    logo: '/9.svg',
   },
   {
-    logo : '/10.svg',
+    logo: '/10.svg',
   },
   {
-    logo : '/6.svg',
+    logo: '/6.svg',
   },
   {
-    logo : '/7.svg',
+    logo: '/7.svg',
   },
   {
-    logo : '/8.svg',
+    logo: '/8.svg',
   },
 ]
 
@@ -256,50 +256,50 @@ export const mentors = [
 ]
 export const authors = [
   {
-    name:"Deepak Kumar",
-    profile:"https://ik.imagekit.io/zwxa4kttt/1661931118137.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1677357864998",
-    job:"Software Engineer - II",
+    name: "Deepak Kumar",
+    profile: "https://ik.imagekit.io/zwxa4kttt/1661931118137.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1677357864998",
+    job: "Software Engineer - II",
     description: "This is an awesome resource. I had been interviewing for past 4 years with no luck. I studied the material for"
   },
   {
-    name:"Venkatesh Gupta",
-    profile:"https://ik.imagekit.io/zwxa4kttt/1621098083610.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1677357898740",
-    job:"Product Manager",
+    name: "Venkatesh Gupta",
+    profile: "https://ik.imagekit.io/zwxa4kttt/1621098083610.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1677357898740",
+    job: "Product Manager",
     description: "Xplainerr's course was incredibly helpful in getting me up to speed quickly for product management interviews. Rather..."
   },
 ]
 
 export const offers = [
   {
-    description:"In-depth understanding of Google Ads, Keyword research & intent mapping to become a better marketer"
+    description: "In-depth understanding of Google Ads, Keyword research & intent mapping to become a better marketer"
   },
   {
-    description:"Complete guide and tested hacks to write search ad copies that convert"
+    description: "Complete guide and tested hacks to write search ad copies that convert"
   },
   {
-    description:"Skills to set up & execute Google Shopping Campaign from scratch to become a better marketer"
+    description: "Skills to set up & execute Google Shopping Campaign from scratch to become a better marketer"
   },
   {
-    description:"Understanding of multiple types of Google Ads Campaigns to generate high-quality leads"
+    description: "Understanding of multiple types of Google Ads Campaigns to generate high-quality leads"
   },
   {
-    description:"Skills & hacks to measure & optimize marketing performance through Google Ads & Analytics"
+    description: "Skills & hacks to measure & optimize marketing performance through Google Ads & Analytics"
   },
   {
-    description:"Best practices for UTM tags, SKAG, & Structuring Google Ad Account to become a better marketer"
+    description: "Best practices for UTM tags, SKAG, & Structuring Google Ad Account to become a better marketer"
   },
   {
-    description:"Increase your service offerings as a freelance marketer"
+    description: "Increase your service offerings as a freelance marketer"
   },
   {
-    description:"Roadmap to shift your career to digital marketing from any industry from scratch"
+    description: "Roadmap to shift your career to digital marketing from any industry from scratch"
   },
 ]
 
 export const certificateTexts = [
   {
     description: "Receive an instructor signed certificate with the institution’s logo to verify your achievements and increase your job prospects",
-    icon:'Ci1.svg'
+    icon: 'Ci1.svg'
   },
   {
     description: "Add the certificate to your CV or your resume or post it directly on LinkedIn. You can even post it on Instagram and Twitter.",
@@ -313,102 +313,102 @@ export const certificateTexts = [
 
 export const footerColOne = [
   {
-    slug:'1',
-    text : "Link One",
+    slug: '1',
+    text: "Link One",
   },
   {
-    slug:'2',
-    text : "Link Two",
+    slug: '2',
+    text: "Link Two",
   },
   {
-    slug:'3',
-    text : "Link Three",
+    slug: '3',
+    text: "Link Three",
   },
   {
-    slug:'4',
-    text : "Link Four",
+    slug: '4',
+    text: "Link Four",
   },
   {
-    slug:'5',
-    text : "Link Five",
+    slug: '5',
+    text: "Link Five",
   },
 ]
 export const footerColTwo = [
   {
-    slug:'6',
-    text : "Link Six",
+    slug: '6',
+    text: "Link Six",
   },
   {
-    slug:'7',
-    text : "Link Seven",
+    slug: '7',
+    text: "Link Seven",
   },
   {
-    slug:'8',
-    text : "Link Eight",
+    slug: '8',
+    text: "Link Eight",
   },
   {
-    slug:'9',
-    text : "Link Nine",
+    slug: '9',
+    text: "Link Nine",
   },
   {
-    slug:'10',
-    text : "Link Ten",
+    slug: '10',
+    text: "Link Ten",
   },
 ]
 export const footerColThree = [
   {
-    slug:'11',
-    text : "Link Eleven",
+    slug: '11',
+    text: "Link Eleven",
   },
   {
-    slug:'12',
-    text : "Link Twelve",
+    slug: '12',
+    text: "Link Twelve",
   },
   {
-    slug:'13',
-    text : "Link Thirteen",
+    slug: '13',
+    text: "Link Thirteen",
   },
   {
-    slug:'13',
-    text : "Link Fourteen",
+    slug: '13',
+    text: "Link Fourteen",
   },
   {
-    slug:'15',
-    text : "Link Fifteen",
+    slug: '15',
+    text: "Link Fifteen",
   },
 ]
 
 export const policy = [
   {
-    slug :'',
-    text:"Privacy Policy"
+    slug: '',
+    text: "Privacy Policy"
   },
   {
-    slug :'',
-    text:"Terms of Service "
+    slug: '',
+    text: "Terms of Service "
   },
   {
-    slug :'',
-    text:"Cookies Settings"
+    slug: '',
+    text: "Cookies Settings"
   },
 ]
 
 export const upgradeItem = [
   {
-    title:"Entire course library",
-    des:"Get full access to our course library, with hours of lessons and example answers."
+    title: "Entire course library",
+    des: "Get full access to our course library, with hours of lessons and example answers."
   },
   {
-    title:"Mock interview videos",
-    des:"Watch experts answer real interview questions in hundreds of videos."
+    title: "Mock interview videos",
+    des: "Watch experts answer real interview questions in hundreds of videos."
   },
   {
-    title:"Private Slack access",
-    des:"Join our daily discussions with 10,000 other Exponent members on Slack."
+    title: "Private Slack access",
+    des: "Join our daily discussions with 10,000 other Exponent members on Slack."
   },
   {
-    title:"Live daily practice",
-    des:"With our new peer practice tool, you can practice with other members every day."
+    title: "Live daily practice",
+    des: "With our new peer practice tool, you can practice with other members every day."
   }
 ]
 
@@ -417,85 +417,85 @@ export const myCourses = [
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
   {
     title: 'API for Product Manager',
     coverImage: 'myCourse.svg',
     ratings: "",
-    instructor:"Deepak Kumar",
-    progress : 10
+    instructor: "Deepak Kumar",
+    progress: 10
   },
 ];
 
@@ -598,6 +598,137 @@ export const courseContent = [
         slug: ''
       },
     ]
+  },
+]
+
+export const allCourse = [
+  {
+    title: 'UX Writing',
+    coverImage: "excel.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '4.0',
+    learners: "3,000",
+    slug: "",
+  },
+  {
+    title: 'Docker Fundamentals',
+    coverImage: "data-science.png",
+    category: "Programming",
+    categorySlug: "programming",
+    ratings: '4.5',
+    learners: "2,000",
+    slug: "",
+  },
+  {
+    title: 'API For Product Manager',
+    coverImage: "excel.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '5.0',
+    learners: "40,000",
+    slug: "",
+  },
+  {
+    title: 'Master ChatGPT',
+    coverImage: "digital-marketing.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '4.0',
+    learners: "25,000",
+    slug: "",
+  },
+  {
+    title: 'Be a Github Pro',
+    coverImage: "c-plus-plus.png",
+    category: "Programming",
+    categorySlug: "programming",
+    ratings: '3.5',
+    learners: "12,000",
+    slug: "",
+  },
+  {
+    title: 'A to Z Of Pricing',
+    coverImage: "digital-marketing.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '5.0',
+    learners: "30,000",
+    slug: "",
+  },
+  {
+    title: 'Postman 101',
+    coverImage: "data-science.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '4.5',
+    learners: "34,000",
+    slug: "",
+  },
+  {
+    title: 'API For Product Manager',
+    coverImage: "digital-marketing.png",
+    category: "Product Management",
+    categorySlug: "product-management",
+    ratings: '5.0',
+    learners: "40,000",
+    slug: "",
+  },
+  {
+    title: 'JavaScript Fundamentals',
+    coverImage: "c-plus-plus.png",
+    category: "Programming",
+    categorySlug: "programming",
+    ratings: '4.0',
+    learners: "3,003",
+    slug: "",
+  },
+]
+
+export const courseCategories = [
+  {
+    name: "Most Popular",
+    slug: "most-popular",
+  },
+  {
+    name: "Product Management",
+    slug: "product-management",
+  },
+  {
+    name: "Programming",
+    slug: "programming",
+  },
+  {
+    name: "Business & Management",
+    slug: "business-Management",
+  },
+  {
+    name: "Core Engineering",
+    slug: "core-engineering",
+  },
+  {
+    name: "Design",
+    slug: "design",
+  },
+  {
+    name: "Creative Arts",
+    slug: "creative-arts",
+  },
+  {
+    name: "Language",
+    slug: "language",
+  },
+  {
+    name: "Career Development",
+    slug: "career-development",
+  },
+  {
+    name: "Architecture",
+    slug: "architecture",
+  },
+  {
+    name: "Online Courses",
+    slug: "online-courses",
   },
 ]
 
