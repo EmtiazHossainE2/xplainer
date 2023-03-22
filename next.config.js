3cb704e1dedc94f3ad05417e54608a2cc74e98c5
Deepak Kumar <deepak@airmeet.com>
2023-03-22 14:56:06 +0530

    updated nextjs config for env variables

--- next.config.js
+++ next.config.js
@@ -2,32 +2,47 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img.freepik.com",'i.ytimg.com',"lh3.googleusercontent.com",]
+    domains: ["img.freepik.com", "i.ytimg.com", "lh3.googleusercontent.com"],
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
+        protocol: "https",
+        hostname: "img.freepik.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
+        protocol: "https",
+        hostname: "i.ytimg.com",
       },
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'media.giphy.com',
+        protocol: "https",
+        hostname: "media.giphy.com",
       },
     ],
+    env: {
+      NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+        process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+        process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER:
+        process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER,
+      NEXT_PUBLIC_FIREBASE_APP_ID:
+        process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID:
+        process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
