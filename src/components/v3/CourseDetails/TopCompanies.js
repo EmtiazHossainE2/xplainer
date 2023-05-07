350832a18b0bc327581ffccc81ae31387f505f2f
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-05-07 23:52:57 +0600

    logo update

--- src/components/v3/CourseDetails/TopCompanies.js
+++ src/components/v3/CourseDetails/TopCompanies.js
@@ -1,25 +1,37 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const TopCompanies = ({ topCompanies }) => {
   return (
-    <div className="border p-5">
-      <h2 className="pb-5 text-base font-bold text-[#1C1D1F] lg:pb-1 lg:text-xl">
+    <div className="border py-5 px-0 lg:px-5">
+      <h2 className="pb-5 text-base text-center lg:text-start font-bold text-[#1C1D1F] lg:pb-1 lg:text-xl">
         Learners from top companies
       </h2>
-      <div className="flex flex-wrap justify-between  gap-3">
+      <div className="flex flex-wrap justify-center lg:justify-between  gap-3">
         {topCompanies.map((logo, index) => (
-          <Image
-            key={index}
-            //****************  logo will be dynamic  *****************/
-            src={`/images/courses/v3/companyLogo/${logo.logo}`}
-            width={80}
-            height={80}
-            alt="logo"
-          />
+          <>
+            <Image
+              key={index}
+              //****************  logo will be dynamic  *****************/
+              src={`/images/brand/${logo.logo}`}
+              width={80}
+              height={80}
+              alt="logo"
+              className="hidden lg:block"
+            />
+            <Image
+              key={index}
+              //****************  logo will be dynamic  *****************/
+              src={`/images/brand/${logo.logo}`}
+              width={60}
+              height={60}
+              alt="logo"
+              className="lg:hidden"
+            />
+          </>
         ))}
       </div>
     </div>
   );
 };
 
-export default TopCompanies
+export default TopCompanies;
