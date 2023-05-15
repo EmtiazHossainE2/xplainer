9e9fe64554c11de36f6e0048273cac283be03f03
Deepak Kumar <deepak@airmeet.com>
2023-05-15 15:22:28 +0530

    updated data

--- src/layout/PageLayout.js
+++ src/layout/PageLayout.js
@@ -1,36 +1,21 @@
-import { Router, withRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { withRouter } from "next/router";
+import { useState } from "react";
+import CommonHead from "../components/v1/Shared/CommonHead";
 import Footer2 from "../components/v1/Shared/Footer/Footer";
-import Navbar from "../components/v3/Shared/Navbar/Navbar";
-import Cookies from 'js-cookie'
-import { useDispatch } from "react-redux";
-import { logout } from "../store/features/auth/authSlice";
 import { CouponModal, LeadModal } from "../components/v1/Shared/Modal";
-
+import Navbar from "../components/v3/Shared/Navbar/Navbar";
 
 const PageLayout = ({ children, router }) => {
   const [showPopup, setShowPopup] = useState(false);
   const [couponModal, setCouponModal] = useState(false);
 
-  const dispatch =  useDispatch();
-
-  /** Protected Page Layout  */
-  // useEffect(()=> {
-  //   const user = Cookies.get('user');
-  //   if(!user){
-  //     dispatch(logout());
-  //     router.push('/')
-  //   }
-  // }, [dispatch]);
-
   return (
     <>
-      {/* <Navbar router={router}/> */}
+      <CommonHead />
+
       <Navbar router={router} />
 
       {children}
-
-      {/* <Footer /> */}
 
       <Footer2 />
 
