709eb6482ff8a5f79af5f5be04032316e223a7a2
Deepak Kumar <deepak@airmeet.com>
2023-05-25 22:27:31 +0530

    updated course learning view title

--- src/components/v1/Shared/ContentView/CourseLearningView.js
+++ src/components/v1/Shared/ContentView/CourseLearningView.js
@@ -1,33 +1,47 @@
-const { default: SidebarLayout } = require("@/src/layout/SidebarLayout")
-const { default: Footer2 } = require("../Footer/Footer")
-const { default: UpgradeToPremium } = require("../UpgradeToPremium")
-const { default: ContentLayout } = require("./ContentLayout")
-const CourseLearningView = ({courseNavigationData, course, slug, hasAccess, isFreeChapter, frontmatter, content}) => {
+import Head from "next/head";
 
-    const shouldUnlock = hasAccess || isFreeChapter;
+const { default: SidebarLayout } = require("@/src/layout/SidebarLayout");
+const { default: Footer2 } = require("../Footer/Footer");
+const { default: UpgradeToPremium } = require("../UpgradeToPremium");
+const { default: ContentLayout } = require("./ContentLayout");
+const CourseLearningView = ({
+  courseNavigationData,
+  course,
+  slug,
+  hasAccess,
+  isFreeChapter,
+  frontmatter,
+  content,
+}) => {
+  const shouldUnlock = hasAccess || isFreeChapter;
 
-    return (
-        <div>
-        <SidebarLayout posts={courseNavigationData} course={course} slug={slug}>
-          {shouldUnlock ? (
-            <div className="p-4">
-              <div>
-                <h1 className="post-heading pb-3">{frontmatter?.title}</h1>
-                <hr className="pb-3" />
-              </div>
-              <div className="blog__content text-align-justify mb-5">
-                <ContentLayout content={content} />
-              </div>
+  return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{`${frontmatter?.title} - Xplainerr`}</title>
+      </Head>
+
+      <SidebarLayout posts={courseNavigationData} course={course} slug={slug}>
+        {shouldUnlock ? (
+          <div className="p-4">
+            <div>
+              <h1 className="post-heading pb-3">{frontmatter?.title}</h1>
+              <hr className="pb-3" />
             </div>
-          ) : (
-            <>
-              <UpgradeToPremium posts={courseNavigationData} course={course} />
-            </>
-          )}
-          <Footer2 />
-        </SidebarLayout>
-      </div>
-    )
-}
+            <div className="blog__content text-align-justify mb-5">
+              <ContentLayout content={content} />
+            </div>
+          </div>
+        ) : (
+          <>
+            <UpgradeToPremium posts={courseNavigationData} course={course} />
+          </>
+        )}
+        <Footer2 />
+      </SidebarLayout>
+    </div>
+  );
+};
 
 export default CourseLearningView;
